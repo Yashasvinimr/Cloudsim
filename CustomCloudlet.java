2a149be7106af291a44a4f14e5f7ca9ffc107160
Yashasvinimr <117368989+Yashasvinimr@users.noreply.github.com>
2024-12-30 22:28:42 +0530

    Update CustomCloudlet.java

--- CustomCloudlet.java
+++ CustomCloudlet.java
@@ -1,27 +1,63 @@
 package org.cloudbus.cloudsim.examples;
-
+//
+//import org.cloudbus.cloudsim.Cloudlet;
+//import org.cloudbus.cloudsim.UtilizationModel;
+//
+//public class CustomCloudlet extends Cloudlet {
+//
+//    private long arrivalTime;
+//
+//    public CustomCloudlet(int id, long length, int pes, long fileSize, long outputSize,
+//                          UtilizationModel utilizationModel, UtilizationModel utilizationModel2,
+//                          UtilizationModel utilizationModel3) {
+//        super(id, length, pes, fileSize, outputSize, utilizationModel, utilizationModel2, utilizationModel3);
+//    }
+//
+//    // Getter and Setter for arrivalTime
+//    public long getArrivalTime() {
+//        return arrivalTime;
+//    }
+//
+//    public void setArrivalTime(long arrivalTime) {
+//        this.arrivalTime = arrivalTime;
+//    }
+//
+//}
 import org.cloudbus.cloudsim.Cloudlet;
 import org.cloudbus.cloudsim.UtilizationModel;
 
 public class CustomCloudlet extends Cloudlet {
-    private int priority;  // Add a priority field
+    private double remainingLength;
+    private long arrivalTime; // Already present
+    private double finishTime;
 
-    // Constructor to initialize the Cloudlet with priority
-    public CustomCloudlet(int cloudletId, long cloudletLength, int pesNumber, long cloudletFileSize,
-                          long cloudletOutputSize, UtilizationModel utilizationModelCpu,
-                          UtilizationModel utilizationModelRam, UtilizationModel utilizationModelBw,
-                          int priority) {
-        super(cloudletId, cloudletLength, pesNumber, cloudletFileSize, cloudletOutputSize,
-                utilizationModelCpu, utilizationModelRam, utilizationModelBw);
-        this.priority = priority;
+    public CustomCloudlet(int cloudletId, long cloudletLength, int pesNumber, long cloudletFileSize, long cloudletOutputSize,
+                          UtilizationModel utilizationModelCpu, UtilizationModel utilizationModelRam, UtilizationModel utilizationModelBw) {
+        super(cloudletId, cloudletLength, pesNumber, cloudletFileSize, cloudletOutputSize, utilizationModelCpu, utilizationModelRam, utilizationModelBw);
+        this.remainingLength = cloudletLength; // Initialize remaining length
     }
 
-    // Getter and Setter for priority
-    public int getPriority() {
-        return priority;
+    public double getRemainingLength() {
+        return remainingLength;
     }
 
-    public void setPriority(int priority) {
-        this.priority = priority;
+    public void setRemainingLength(double remainingLength) {
+        this.remainingLength = remainingLength;
+    }
+
+    public long getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(long arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(double finishTime) {
+        this.finishTime = finishTime;
     }
 }
